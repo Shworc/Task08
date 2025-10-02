b80aa0b407a509625380e54fd581e186f5414453
Sasa Filipovic <sasa-f@hotmail.com>
2025-10-02 13:51:05 +0200

    size v6

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name            = "system"
     node_count      = 1
-    vm_size         = "Standard_D2ads_v5"
+    vm_size         = "Standard_D2ads_v6"
     os_disk_type    = "Ephemeral"
     os_disk_size_gb = 75
 
